ff82d2e3a68b8df9acbb3c133bbb123f83a8affa
JVCHENG <JVCHENG@10.10.12.213>
2019-05-06 16:46:34 +0800

    删除Test

--- FileProcessing/src/explore/third/Test.java
+++
@@ -1,9 +0,0 @@
-package explore.third;
-
-import org.apache.maven.shared.model.fileset.Mapper;
-
-public class Test {
-	public static void main(String[] args) {
-		Mapper mapper=new Mapper();
-	}
-}
